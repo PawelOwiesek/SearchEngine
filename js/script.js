cc79275a9e60fd3a7d54fd45123a7a34ba798021
Pawel Owiesek <Owiesek.p@gmail.com>
2023-05-16 11:10:47 +0200

    code formated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 {
-    const search = document.querySelector(".js-search");
-    const listItems = document.querySelectorAll(".js-listItem");
+  const search = document.querySelector(".js-search");
+  const listItems = document.querySelectorAll(".js-listItem");
 
-    const onImputAction = () => {
-        listItems.forEach((listItem) => {
-            const match = new RegExp(search.value, "i").test(listItem.textContent);
-            match ? listItem.style.display = "block" :
-                listItem.style.display = "none";
-        });
-    };
-    const init = () => {
-        search.addEventListener("keyup", onImputAction);
-        onImputAction();
-    };
-    init();
+  const onImputAction = () => {
+    listItems.forEach((listItem) => {
+      const match = new RegExp(search.value, "i").test(listItem.textContent);
+      match
+        ? (listItem.style.display = "block")
+        : (listItem.style.display = "none");
+    });
+  };
+  const init = () => {
+    search.addEventListener("keyup", onImputAction);
+    onImputAction();
+  };
+  init();
 }
